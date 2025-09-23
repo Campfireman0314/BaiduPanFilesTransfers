3e7d06be3fa66e6231a92b8f624eed971d9b0a93
grp.tfaudio <grp.tfaudio@corp.netease.com>
2025-09-23 17:27:55 +0800

    Update

--- BaiduPanFilesTransfers.py
+++ BaiduPanFilesTransfers.py
@@ -8,6 +8,8 @@
 :copyright: Copyright 2024, hxz393. 保留所有权利。
 """
 
+import os
+import shutil
 from src.operations import Operations
 from src.ui import MainWindow
 import traceback
@@ -19,6 +21,7 @@ import ctypes
 from bypy import ByPy
 from flask import Flask, request, g, current_app
 from flask_cors import CORS
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -47,7 +50,7 @@ def responser() -> str:
     ext_link_str = data.get('message')  # 获取关键字之后的内容
     popo_group_number = data.get('group')
 
-    # Judge if a previous thread is not finished.
+    # Judge if a previous thread is not finished
     global t
     if t.is_alive():
         if ext_link_str == '做掉':
@@ -58,7 +61,7 @@ def responser() -> str:
                     thread_id = _id
                     break
 
-            # Raise exception to force terminate.
+            # Raise exception to force terminate
             res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
                 thread_id,
                 ctypes.py_object(SystemExit)
@@ -70,6 +73,19 @@ def responser() -> str:
                 return ""
 
             t.join()
+
+            # Remove old files in cloud
+            bp = ByPy()
+            bp.rm('/fetch')
+            bp.list()
+
+            # Remove files in local folder
+            global current_working_local_dir
+            if os.path.isdir(current_working_local_dir):
+                shutil.rmtree(current_working_local_dir)
+                current_working_local_dir = ''
+
+
             send_popo_alert(popo_group_number, '*远处传来一声模糊的枪响……*')
 
         else:
@@ -84,18 +100,20 @@ def responser() -> str:
     return ""
 
 
+
+remote_dir = 'apps/bypy/fetch'
+local_dir = '//10.246.77.61/audio/项目资源/【L36】 逆水寒手游/资源管理专区/录音回收/自动回收'
+current_working_local_dir = ''
+
 def main(popo_group_number, ext_link_str) -> None:
     """
     主函数，先创建主窗口实例，然后创建动作实例，更新主窗口实例中的动作对象引用，最后运行.
 
     :return: 无返回值
     """
-
-    remote_dir = 'apps/bypy'
-    local_dir = 'Q:/ByPy'
     # ext_link_str = 'https://pan.baidu.com/s/1wCc8tW9DVLhQx0VC80g6HQ?pwd=3333'
-    cookie = 'XFI=1dd30f97-b706-9a91-4e26-41c9db85a645; XFCS=8E3F88CE55D4206389385FD7C5B387BEEB8CDDF4FEE7D8F3D07849F2B3075F03; XFT=R3iCfceZ3X9Ra4u5rOYBWoEeJ/pyUASNgDaPtA6Q79Y=; BIDUPSID=317B888C802554BA5A4DC433FEA0F083; PSTM=1658452399; __bid_n=18bd7d682c97456acafc87; ZFY=Z4v:AkDUo6Bwei58A:Al:B7RjeLhBQyKmi764cjgr5xd4g:C; newlogin=1; H_PS_PSSID=60450_60840; BA_HECTOR=0h2g01a40h2l00a4250g21ak12f9kr1jgcdci1u; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; RT="z=1&dm=baidu.com&si=56405693-60ee-484e-80c5-f6e3d492b399&ss=m21kn06g&sl=2&tt=17r&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=a7n&ul=2az3&hd=2azm"; BAIDUID=7327D64C53BEF1D6054EB460AFDC1173:FG=1; BAIDUID_BFESS=7327D64C53BEF1D6054EB460AFDC1173:FG=1; BDUSS=BldmZqcWNwYXpNVjV5cG1ER1hrdFdUQTBLNXJ4cGF2TXFwYjBnZzVydVc2QzFuSVFBQUFBJCQAAAAAAAAAAAEAAAAw40c1tefE1L~xyMtmcmVlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJZbBmeWWwZnYz; BDUSS_BFESS=BldmZqcWNwYXpNVjV5cG1ER1hrdFdUQTBLNXJ4cGF2TXFwYjBnZzVydVc2QzFuSVFBQUFBJCQAAAAAAAAAAAEAAAAw40c1tefE1L~xyMtmcmVlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJZbBmeWWwZnYz; STOKEN=d8b157e27c0e337e3af1dca45b9a17999a62fdef68d4b096a394d19cecdd272f; PANWEB=1; Hm_lvt_95fc87a381fad8fcb37d76ac51fefcea=1728460284,1728477355; csrfToken=ONKOSPcFD0jfsO5STPw1UC2j; ndut_fmt=2DD169B3FF09D18C21D218623F818D6F2E2DFF3EA9245DD0B779CA8883638DEC; ab_sr=1.0.1_NjZhMWU5ZmQyNjZjN2JjYWJhZTljNTJlNjZiZWZkOTI2MjA5N2MwZDhkODYwMDg0NzBkZjYxNTI4ZmEwZTQzN2U4OTUyMTkzMDI4NTRlODE2NWM2Y2U3YmZkN2EzZDY4ZDE1M2UyOTE3ZDU5NGI1ZTVkODZiYTdkMTZlYzNjZWI5ZmEwZjVmNWZmZTZhOGFjZjgzNmUzNTE5NTE1MmY3NzZlMTc3YzE2NjgxMTJiNWQyM2JkZjA2M2Q0NTRlOWI0; PANPSC=12162860685606541493%3APBu5R64BDn0wlm%2FIJ26EF1cS2d9ns3O5CM3QoQJ7YXfUUERun5%2BOPd3tsxK%2FuXovCOfgdHnp14%2Blq3aOdzuDDWYa2dyKURQjFl7owtXleijihEIzmOtMnRn%2BOGYsIE0xYqZdy9zZ13kc816WHUcopqIT9ar9kqyBEct689Zi52EbJknp%2BIbkWgf7w8OoZQjac0Z4JFwNQ0FFEIreWjytXwU0coV4hk9L2z8xw%2B8jjD0uiVtc73OZBmRHELtki1kid4u3tn0BShySsVZaqyA8TA%3D%3D'
-
+    cookie = 'XFI=6f7d30b4-5291-c9cf-8486-eabe487ec93c; XFCS=2A2A6A6094F35F23400A14CEC6019E93ABF98F2B57A95F08076FFE6AAE2D28C4; XFT=xnVenE2c71PeZM95rTxLr5SX4bRHfYWwBuOqFer5FRc=; BDUSS=5TRVFRa2VadWdVSXM3RjI4N3g1VElMRzF3U0JXenhIWDRKTnYyQ0Q2bXh-WGxvSVFBQUFBJCQAAAAAAAAAAAEAAAAw40c1tefE1L~xyMtmcmVlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALFwUmixcFJock; BDUSS_BFESS=5TRVFRa2VadWdVSXM3RjI4N3g1VElMRzF3U0JXenhIWDRKTnYyQ0Q2bXh-WGxvSVFBQUFBJCQAAAAAAAAAAAEAAAAw40c1tefE1L~xyMtmcmVlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALFwUmixcFJock; csrfToken=e_PAl1CiPvMsXe9EykWgGJMj; BAIDUID=B82ECD8D5860A76747EB516355CADEBA:FG=1; BAIDUID_BFESS=B82ECD8D5860A76747EB516355CADEBA:FG=1; STOKEN=ab4c85ad350ec0ff6f7473bcfbef730547660fbcebb081b9ffca20bea3f972dc; PANPSC=4318866266134880047%3AnHZOtuqy9avP0ni4p%2BWoLFcS2d9ns3O5C61tf8CKQkiKoBW5QeZJvSA9YKyMd6JEIi%2Fo2hbQKHo4EhHQlt8W9eNHiOwr%2ByPWOg9ifdcaUVefh9sBDMQOvZepI3x%2F4x9XJlOfPZFHq34iVxZWCjKhbJI6enfTG9RjNaEJrI5V%2Fed8Wde7ocAHCFvKX4DMNpv6FHFBPAHjbTGpxI5LM2ldbsk9%2F%2BQGNO%2B6ZyiEnGgQhUpQz1Ave%2FXPCIpK9cDD9h6M8obwdXYAHVBqWoMK8sXNY9WMPddrrScAmDZZmhId7d5joGCvqKhsU%2FMgNQEH8kn3krFWWqsgPEySsVZaqyA8TA%3D%3D; Hm_lvt_182d6d59474cf78db37e0b2248640ea5=1757908196; Hm_lpvt_182d6d59474cf78db37e0b2248640ea5=1757908196; ndut_fmt=B05E9FF3990CA0B257872B3ECD2553E0E51DF9868AF76FD03DE611F31E69603A; ab_sr=1.0.1_ZjM1MmY0OGY1MTU5MzNhMDhhYjIyZWUzZWVmYzMzY2I4MTgzOTIzNTljYWFjYTJkZjRmYWNhNWNkZDhjZmU0NWFlMGZlYjgwYTU4ZmUyZDk5M2M0MjhjZGFhOWFmOGQ0NTU2MDY0ZDRmOTg4NzQ2ZDNjYTFlMThmZjVlMWQzYzczMWFkNmQyNGU5N2Q4N2UzODcyNTI2ODNlYTAyZjZiMDM1NzRmODg2MTQyOTE1YWE4OTkzZjYyYzg4NmIyZjM5'
+    
     # 创建主窗口实例，先传入 None 占位
     root = MainWindow(None)
     # 创建逻辑处理对象并传递主窗口实例
@@ -105,10 +123,13 @@ def main(popo_group_number, ext_link_str) -> None:
     # root.op = op
     # root.run()
 
-    # Ideal logic:
+    global remote_dir
+    global local_dir
+
     try:
         op.prepare_run_ext(cookie, remote_dir)
-        op.setup_save_ext(ext_link_str)
+        if op.setup_save_ext(ext_link_str) == False:
+            raise Exception('Invalid input')
         op.handle_input()
         op.handle_bdstoken()
         op.handle_create_dir(folder_name=op.folder_name)
@@ -123,19 +144,31 @@ def main(popo_group_number, ext_link_str) -> None:
         op.network.s.close()
         op.change_status_ext('stopped')
 
+    bp = ByPy()
+    global current_working_local_dir
     try:
-        bp = ByPy()
         bp.list()
-        bp.downdir('/', local_dir)
+        now = datetime.now() # current date and time
+        date_time = now.strftime("%Y%m%d_%H%M%S")
+        current_working_local_dir = local_dir + '/' + date_time
+        if not os.path.isdir(current_working_local_dir):
+            os.mkdir(current_working_local_dir)
+        bp.downdir('/fetch', current_working_local_dir)
+        bp.rm('/fetch')
         send_popo_alert(popo_group_number, '*小灵通上是狗腿子的来电：“‘机长’，我到了，老地方。”*')
     except Exception as e:
         op.insert_logs(
             f'程序出现未预料错误，信息如下：\n{e}\n{traceback.format_exc()}', False, 3)
+        bp.rm('/fetch')
+        if os.path.isdir(current_working_local_dir):
+            shutil.rmtree(current_working_local_dir)
+            current_working_local_dir = ''
         send_popo_alert(popo_group_number, '*小灵通上是狗腿子的来电：“‘机长’，钱被截了”*')
         return
 
 
-
 if __name__ == '__main__':
     # main()
-    app.run('0.0.0.0', port=1001)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=1001)
+    #app.run('0.0.0.0', port=1001)

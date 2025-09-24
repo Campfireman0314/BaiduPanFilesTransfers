9716a50a7d6ec96a617588c844fb159d1336898d
linyudong <linyudong@corp.netease.com>
2025-09-24 16:52:50 +0800

    config replacement

--- BaiduPanFilesTransfers.py
+++ BaiduPanFilesTransfers.py
@@ -22,11 +22,11 @@ from bypy import ByPy
 from flask import Flask, request, g, current_app
 from flask_cors import CORS
 from datetime import datetime
-
+from config import *
 
 app = Flask(__name__)
-CORS(app, resources=r'/*')	 # 注册CORS, "/*" 允许访问所有api
-# CORS(app,  resources={r"/*": {"origins": "*.163.com"}})   # 允许163后缀域名跨域
+# CORS(app, resources=r'/*')	 # 注册CORS, "/*" 允许访问所有api
+CORS(app,  resources={r"/*": {"origins": "*.163.com"}})   # 允许163后缀域名跨域
 t = threading.Thread()
 
 funny_reply = [
@@ -99,10 +99,6 @@ def responser() -> str:
 
     return ""
 
-
-
-remote_dir = 'apps/bypy/fetch'
-local_dir = '//10.246.77.61/audio/项目资源/【L36】 逆水寒手游/资源管理专区/录音回收/自动回收'
 current_working_local_dir = ''
 
 def main(popo_group_number, ext_link_str) -> None:
@@ -112,7 +108,6 @@ def main(popo_group_number, ext_link_str) -> None:
     :return: 无返回值
     """
     # ext_link_str = 'https://pan.baidu.com/s/1wCc8tW9DVLhQx0VC80g6HQ?pwd=3333'
-    cookie = 'XFI=6f7d30b4-5291-c9cf-8486-eabe487ec93c; XFCS=2A2A6A6094F35F23400A14CEC6019E93ABF98F2B57A95F08076FFE6AAE2D28C4; XFT=xnVenE2c71PeZM95rTxLr5SX4bRHfYWwBuOqFer5FRc=; BDUSS=5TRVFRa2VadWdVSXM3RjI4N3g1VElMRzF3U0JXenhIWDRKTnYyQ0Q2bXh-WGxvSVFBQUFBJCQAAAAAAAAAAAEAAAAw40c1tefE1L~xyMtmcmVlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALFwUmixcFJock; BDUSS_BFESS=5TRVFRa2VadWdVSXM3RjI4N3g1VElMRzF3U0JXenhIWDRKTnYyQ0Q2bXh-WGxvSVFBQUFBJCQAAAAAAAAAAAEAAAAw40c1tefE1L~xyMtmcmVlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALFwUmixcFJock; csrfToken=e_PAl1CiPvMsXe9EykWgGJMj; BAIDUID=B82ECD8D5860A76747EB516355CADEBA:FG=1; BAIDUID_BFESS=B82ECD8D5860A76747EB516355CADEBA:FG=1; STOKEN=ab4c85ad350ec0ff6f7473bcfbef730547660fbcebb081b9ffca20bea3f972dc; PANPSC=4318866266134880047%3AnHZOtuqy9avP0ni4p%2BWoLFcS2d9ns3O5C61tf8CKQkiKoBW5QeZJvSA9YKyMd6JEIi%2Fo2hbQKHo4EhHQlt8W9eNHiOwr%2ByPWOg9ifdcaUVefh9sBDMQOvZepI3x%2F4x9XJlOfPZFHq34iVxZWCjKhbJI6enfTG9RjNaEJrI5V%2Fed8Wde7ocAHCFvKX4DMNpv6FHFBPAHjbTGpxI5LM2ldbsk9%2F%2BQGNO%2B6ZyiEnGgQhUpQz1Ave%2FXPCIpK9cDD9h6M8obwdXYAHVBqWoMK8sXNY9WMPddrrScAmDZZmhId7d5joGCvqKhsU%2FMgNQEH8kn3krFWWqsgPEySsVZaqyA8TA%3D%3D; Hm_lvt_182d6d59474cf78db37e0b2248640ea5=1757908196; Hm_lpvt_182d6d59474cf78db37e0b2248640ea5=1757908196; ndut_fmt=B05E9FF3990CA0B257872B3ECD2553E0E51DF9868AF76FD03DE611F31E69603A; ab_sr=1.0.1_ZjM1MmY0OGY1MTU5MzNhMDhhYjIyZWUzZWVmYzMzY2I4MTgzOTIzNTljYWFjYTJkZjRmYWNhNWNkZDhjZmU0NWFlMGZlYjgwYTU4ZmUyZDk5M2M0MjhjZGFhOWFmOGQ0NTU2MDY0ZDRmOTg4NzQ2ZDNjYTFlMThmZjVlMWQzYzczMWFkNmQyNGU5N2Q4N2UzODcyNTI2ODNlYTAyZjZiMDM1NzRmODg2MTQyOTE1YWE4OTkzZjYyYzg4NmIyZjM5'
     
     # 创建主窗口实例，先传入 None 占位
     root = MainWindow(None)
@@ -122,9 +117,6 @@ def main(popo_group_number, ext_link_str) -> None:
     # 更新主窗口中的逻辑处理对象引用
     # root.op = op
     # root.run()
-
-    global remote_dir
-    global local_dir
 
     try:
         op.prepare_run_ext(cookie, remote_dir)
@@ -150,7 +142,7 @@ def main(popo_group_number, ext_link_str) -> None:
         bp.list()
         now = datetime.now() # current date and time
         date_time = now.strftime("%Y%m%d_%H%M%S")
-        current_working_local_dir = local_dir + '/' + date_time
+        current_working_local_dir = local_dir + '/' + date_time + "_TEMP_NO_MODIFICATION"
         if not os.path.isdir(current_working_local_dir):
             os.mkdir(current_working_local_dir)
         bp.downdir('/fetch', current_working_local_dir)
